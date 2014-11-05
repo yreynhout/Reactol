3c5a2a50fe082a1e01d55a1a8c8c9801dc3ae600
yreynhout <yves.reynhout@gmail.com>
2014-11-05 15:51:31 +0100

    Added composer

---
+++ src/Reactol.Tests/ReactionComposerTests.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Reactol
+{
+    namespace ReactionComposerTests
+    {
+        [TestFixture]
+        public class WithAnyInstance
+        {
+            private ReactionComposer _sut;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _sut = new ReactionComposer().
+                    Compose(
+                        Enumerable.Repeat(
+                            new object(),
+                            new Random().Next(0, 10)));
+            }
+
+            [Test]
+            public void ImplicitConversionOfNullThrows()
+            {
+                Assert.Throws<ArgumentNullException>(() => { object[] _ = (ReactionComposer) null; });
+            }
+
+            [Test]
+            public void ExplicitConversionOfNullThrows()
+            {
+                Assert.Throws<ArgumentNullException>(() => { var _ = (object[])(ReactionComposer)null; });
+            }
+
+            [Test]
+            public void ComposeMessageCanNotBeNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.Compose((object) null));
+            }
+
+            [Test]
+            public void ComposeMessageArrayCanNotBeNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.Compose((object[])null));
+            }
+
+            [Test]
+            public void ComposeMessageEnumerationCanNotBeNull()
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.Compose((IEnumerable<object>)null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeIfMessageCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeIf(condition, (object)null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeIfMessageArrayCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeIf(condition, (object[])null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeIfMessageEnumerationCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeIf(condition, (IEnumerable<object>)null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeUnlessMessageCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeUnless(condition, (object)null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeUnlessMessageArrayCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeUnless(condition, (object[])null));
+            }
+
+            [TestCase(true)]
+            [TestCase(false)]
+            public void ComposeUnlessMessageEnumerationCanNotBeNull(bool condition)
+            {
+                Assert.Throws<ArgumentNullException>(() => _sut.ComposeUnless(condition, (IEnumerable<object>)null));
+            }
+        }
+
+        [TestFixture]
+        public class WithEmptyInstance
+        {
+            private ReactionComposer _sut;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _sut = new ReactionComposer();
+            }
+
+            [Test]
+            public void MessagesReturnsExpectedResult()
+            {
+                Assert.That(_sut.Messages, Is.Empty);
+            }
+
+            [Test]
+            public void ImplicitConversionToObjectArrayReturnsExpectedResult()
+            {
+                object[] result = _sut;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ExplicitConversionToObjectArrayReturnsExpectedResult()
+            {
+                var result = (object[])_sut;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeMessageReturnsExpectedResult()
+            {
+                var message = new object();
+                var result = _sut.Compose(message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { message }));
+            }
+
+            [Test]
+            public void ComposeMessageArrayReturnsExpectedResult()
+            {
+                var messages = new [] { new object() };
+                var result = _sut.Compose(messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+
+            [Test]
+            public void ComposeMessageEnumerationReturnsExpectedResult()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.Compose((IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+
+            [Test]
+            public void ComposeIfMessageReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeIf(true, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { message }));
+            }
+
+            [Test]
+            public void ComposeIfMessageArrayReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(true, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+
+            [Test]
+            public void ComposeIfMessageEnumerationReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(true, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+
+            [Test]
+            public void ComposeIfMessageReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeIf(false, message).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeIfMessageArrayReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(false, messages).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeIfMessageEnumerationReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(false, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeUnlessMessageReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeUnless(true, message).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeUnlessMessageArrayReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(true, messages).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeUnlessMessageEnumerationReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(true, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.Empty);
+            }
+
+            [Test]
+            public void ComposeUnlessMessageReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeUnless(false, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageArrayReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(false, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageEnumerationReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(false, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(messages));
+            }
+        }
+
+        [TestFixture]
+        public class WithFilledInstance
+        {
+            private object _message;
+            private ReactionComposer _sut;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _message = new object();
+                _sut = new ReactionComposer().Compose(_message);
+            }
+
+            [Test]
+            public void MessagesReturnsExpectedResult()
+            {
+                Assert.That(_sut.Messages, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ImplicitConversionToObjectArrayReturnsExpectedResult()
+            {
+                object[] result = _sut;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ExplicitConversionToObjectArrayReturnsExpectedResult()
+            {
+                var result = (object[])_sut;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeMessageReturnsExpectedResult()
+            {
+                var message = new object();
+                var result = _sut.Compose(message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message, message }));
+            }
+
+            [Test]
+            public void ComposeMessageArrayReturnsExpectedResult()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.Compose(messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] {_message}.Concat(messages)));
+            }
+
+            [Test]
+            public void ComposeMessageEnumerationReturnsExpectedResult()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.Compose((IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }.Concat(messages)));
+            }
+
+            [Test]
+            public void ComposeIfMessageReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeIf(true, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message, message }));
+            }
+
+            [Test]
+            public void ComposeIfMessageArrayReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(true, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }.Concat(messages)));
+            }
+
+            [Test]
+            public void ComposeIfMessageEnumerationReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(true, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }.Concat(messages)));
+            }
+
+            [Test]
+            public void ComposeIfMessageReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeIf(false, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeIfMessageArrayReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(false, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeIfMessageEnumerationReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeIf(false, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeUnless(true, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageArrayReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(true, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageEnumerationReturnsExpectedResultWhenConditionIsSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(true, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var message = new object();
+                var result = _sut.ComposeUnless(false, message).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message, message }));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageArrayReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(false, messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }.Concat(messages)));
+            }
+
+            [Test]
+            public void ComposeUnlessMessageEnumerationReturnsExpectedResultWhenConditionIsNotSatisfied()
+            {
+                var messages = new[] { new object() };
+                var result = _sut.ComposeUnless(false, (IEnumerable<object>)messages).Messages;
+                Assert.That(result, Is.EquivalentTo(new[] { _message }.Concat(messages)));
+            }
+        }
+    }
+}
--- src/Reactol/ReactionComposer.cs
+++ src/Reactol/ReactionComposer.cs
@@ -1,22 +1,161 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Reactol
 {
     /// <summary>
-    /// 
+    ///   Represents a composition of reaction messages.
     /// </summary>
     public class ReactionComposer
     {
-        public ReactionComposer Compose(object message) { }
-        public ReactionComposer Compose(object[] message) { }
-        public ReactionComposer Compose(IEnumerable<object> message) { }
+        private readonly object[] _messages;
 
-        public ReactionComposer ComposeIf(bool condition, object message) { }
-        public ReactionComposer ComposeIf(bool condition, object[] message) { }
-        public ReactionComposer ComposeIf(bool condition, IEnumerable<object> message) { }
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ReactionComposer" /> class.
+        /// </summary>
+        public ReactionComposer()
+        {
+            _messages = new object[0];
+        }
 
-        public ReactionComposer ComposeUnless(bool condition, object message) { }
-        public ReactionComposer ComposeUnless(bool condition, object[] message) { }
-        public ReactionComposer ComposeUnless(bool condition, IEnumerable<object> message) { }
+        ReactionComposer(object[] messages)
+        {
+            _messages = messages;
+        }
+
+        /// <summary>
+        /// Gets the collected reaction messages.
+        /// </summary>
+        public object[] Messages
+        {
+            get { return _messages; }
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="message" />.
+        /// </summary>
+        /// <param name="message">The <see cref="object">message</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="message" /> is <c>null</c>.</exception>
+        public ReactionComposer Compose(object message)
+        {
+            if (message == null) throw new ArgumentNullException("message");
+            return new ReactionComposer(Messages.Concat(new[] { message}).ToArray());
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" />.
+        /// </summary>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer Compose(object[] messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return new ReactionComposer(Messages.Concat(messages).ToArray());
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" />.
+        /// </summary>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer Compose(IEnumerable<object> messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return new ReactionComposer(Messages.Concat(messages).ToArray());
+        }
+        
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="message" /> if the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="message">The <see cref="object">message</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="message" /> is <c>null</c>.</exception>
+        public ReactionComposer ComposeIf(bool condition, object message)
+        {
+            if (message == null) throw new ArgumentNullException("message");
+            return condition ? Compose(message) : this;
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" /> if the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer ComposeIf(bool condition, object[] messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return condition ? Compose(messages) : this;
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" /> if the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer ComposeIf(bool condition, IEnumerable<object> messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return condition ? Compose(messages) : this;
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="message" /> unless the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="message">The <see cref="object">message</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="message" /> is <c>null</c>.</exception>
+        public ReactionComposer ComposeUnless(bool condition, object message)
+        {
+            if (message == null) throw new ArgumentNullException("message");
+            return !condition ? Compose(message) : this;
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" /> unless the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer ComposeUnless(bool condition, object[] messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return !condition ? Compose(messages) : this;
+        }
+
+        /// <summary>
+        ///     Composes this instance with the specified <paramref name="messages" /> unless the condition is satisfied.
+        /// </summary>
+        /// <param name="condition">The condition to satisfy.</param>
+        /// <param name="messages">The <see cref="object">messages</see> to compose with.</param>
+        /// <returns>A new composition of <see cref="object">messages</see>.</returns>
+        /// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="messages" /> are <c>null</c>.</exception>
+        public ReactionComposer ComposeUnless(bool condition, IEnumerable<object> messages)
+        {
+            if (messages == null) throw new ArgumentNullException("messages");
+            return !condition ? Compose(messages) : this;
+        }
+
+        /// <summary>
+        ///     Implicitly converts a composition of <see cref="object">messages</see> to an array of
+        ///     <see cref="object">messages</see>.
+        /// </summary>
+        /// <param name="instance">The instance to convert.</param>
+        /// <returns>An array of <see cref="object">messages</see>.</returns>
+        public static implicit operator object[](ReactionComposer instance)
+        {
+            if (instance == null) throw new ArgumentNullException("instance");
+            return instance.Messages;
+        }
     }
 }
